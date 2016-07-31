95e15f168cacbd7994ea518cb83ae64d11527f8c
Greg Minshall <minshall@acm.org>
2016-07-31 07:54:05 -0700

    *** empty log message ***

--- python/changes.py
+++ python/changes.py
@@ -6,11 +6,11 @@ print(zf)
 zf.namelist()
 doc = zf.open("doc.kml")
 print(doc)
-doc.read()
+data = doc.read()
 
 f = ogr.Open("doc.kml")
 print("this is %s data" % f.GetDriver().GetName())
 print("data source is %s" % f.GetName())
 print("there is/are %d layer(s)" % f.GetLayerCount())
 print("the first layer is named '%s'" % f.GetLayer(0).GetLayerDefn().GetName())
-
+print(f.GetLayerByName("Soberanes").GetName())
