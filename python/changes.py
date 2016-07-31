1145736a85657789abab69183cb64a754c500c09
Greg Minshall <minshall@acm.org>
2016-07-31 10:10:30 -0700

    *** empty log message ***

--- python/changes.py
+++ python/changes.py
@@ -1,12 +1,14 @@
 import osgeo
 from osgeo import ogr
-import zipfile
-zf = zipfile.ZipFile("20160729_Soberanes_IR.kmz", "r")
-print(zf)
-zf.namelist()
-doc = zf.open("doc.kml")
-print(doc)
-data = doc.read()
+
+# it would be nice to read direct from a .kmz (zip) file.  but it
+# doesn't seem like the current gdal/ogr supports that.
+# import zipfile
+# zf = zipfile.ZipFile("20160729_Soberanes_IR.kmz", "r")
+# print(zf)
+# zf.namelist()
+# ndoc = zf.open("doc.kml")
+# data = doc.read()
 
 f = ogr.Open("doc.kml")
 print("this is %s data" % f.GetDriver().GetName())
@@ -14,3 +16,11 @@ print("data source is %s" % f.GetName())
 print("there is/are %d layer(s)" % f.GetLayerCount())
 print("the first layer is named '%s'" % f.GetLayer(0).GetLayerDefn().GetName())
 print(f.GetLayerByName("Soberanes").GetName())
+
+l = f.GetLayerByName("Soberanes")
+
+hpfid = -1
+for featid in range(l.GetFeatureCount()):
+    feature = l.GetFeature(featid);
+    fieldid = feature.GetFieldIndex("Name");
+    print(feature.GetFieldAsString(fieldid));
