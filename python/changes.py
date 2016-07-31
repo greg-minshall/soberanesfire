89eeaf2d22700d6a9e14fb2155e88f5fa55f02f3
Greg Minshall <minshall@acm.org>
2016-07-31 07:18:27 -0700

    *** empty log message ***

--- python/changes.py
+++ python/changes.py
@@ -1,4 +1,5 @@
 import osgeo
+from osgeo import ogr
 import zipfile
 zf = zipfile.ZipFile("20160729_Soberanes_IR.kmz", "r")
 print(zf)
@@ -6,3 +7,10 @@ zf.namelist()
 doc = zf.open("doc.kml")
 print(doc)
 doc.read()
+
+f = ogr.Open("doc.kml")
+print("this is %s data" % f.GetDriver().GetName())
+print("data source is %s" % f.GetName())
+print("there is/are %d layer(s)" % f.GetLayerCount())
+print("the first layer is named '%s'" % f.GetLayer(0).GetLayerDefn().GetName())
+
